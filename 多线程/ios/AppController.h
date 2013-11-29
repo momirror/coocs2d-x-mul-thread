55ce466bc85fe470b4d7811fe7955b98f08cfc2d
tvttvt <471488376@qq.com>
2013-11-29 14:16:31 +0800

    first commit

---
+++ 多线程/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ___AppController.h
+//  多线程
+//
+//  Created by msp on 13-11-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ 多线程/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  ___AppController.h
+//  多线程
+//
+//  Created by msp on 13-11-22.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
